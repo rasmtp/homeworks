8122bb55716fe0c57b5203cf8870193f1ff5104b
rasmtp <146954320+rasmtp@users.noreply.github.com>
2023-10-10 00:19:17 +0200

    Add files via upload

---
+++ Hw3Task3.java
@@ -0,0 +1,7 @@
+public class Hw3Task3 {
+    public static void main(String[] args) {
+        String stroka = "London is the capital of Great Britain";
+        int number = stroka.split(" ").length;
+        System.out.println(number);
+    }
+}
