e9bee9a70cbd09cc34d529c17827b0d60d149500
rasmtp <146954320+rasmtp@users.noreply.github.com>
2023-11-04 14:33:05 +0100

    unfinishedProject

--- Hw4Task3.java
+++ Hw4Task3.java
@@ -1,2 +1,28 @@
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Hw4Task3 {
+    public static void main(String[] args) {
+        int[] setNumbers = new int[2000];
+        Hw4Task3.randomNumbers(setNumbers);
+        System.out.println("Нечётные и заменённые на ноль чётные числа:");
+        for (int i : setNumbers) {
+            System.out.println(i);
+        }
+    }
+
+    public static void randomNumbers(int[] setRandom) {
+        for (int i = 0; i < setRandom.length; i++) {
+            setRandom[i] = ThreadLocalRandom.current().nextInt(1, 10);
+        }
+        System.out.println("Набор случайных чисел:" + Arrays.toString(setRandom));
+    }
+
+    public static void changeEvenNumbersToZero(int[] evenNumbers) {
+        for (int i = 0; i < evenNumbers.length; i++) {
+            if (evenNumbers[i] % 2 == 0) {
+                evenNumbers[i] = 0;
+            }
+        }
+    }
 }
