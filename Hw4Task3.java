f6c47504c474a6eb4ce757a140a7223d41f28d6e
rasmtp <146954320+rasmtp@users.noreply.github.com>
2023-11-05 17:48:20 +0100

    editedProject

--- Hw4Task3.java
+++ Hw4Task3.java
@@ -5,6 +5,7 @@ public class Hw4Task3 {
     public static void main(String[] args) {
         int[] setNumbers = new int[2000];
         Hw4Task3.randomNumbers(setNumbers);
+        changeEvenNumbersToZero(setNumbers);
         System.out.println("Нечётные и заменённые на ноль чётные числа:");
         for (int i : setNumbers) {
             System.out.println(i);
