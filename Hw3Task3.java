d70792492798c719143a1640b9d2221bf1644d15
rasmtp <146954320+rasmtp@users.noreply.github.com>
2024-02-20 13:57:32 +0100

    Delete Hw3Task3.java

--- Hw3Task3.java
+++
@@ -1,7 +0,0 @@
-public class Hw3Task3 {
-    public static void main(String[] args) {
-        String stroka = "London is the capital of Great Britain";
-        int number = stroka.split(" ").length;
-        System.out.println(number);
-    }
-}
